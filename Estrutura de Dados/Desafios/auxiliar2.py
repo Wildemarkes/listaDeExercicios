344a5b00b0a49e95bdd737f3b2305f69a788b1f1
Wildemarkes <wildemarkesalmeida@gmail.com>
2022-04-14 23:45:32 -0300

    alteração da interface do jogo final

--- Estrutura de Dados/Desafios/auxiliar2.py
+++ Estrutura de Dados/Desafios/auxiliar2.py
@@ -1,49 +1,105 @@
-def binvet(n_max):  # Cria uma lista de Binários
-    a = []
-    for i in range(0, n_max):
-        a.append("{0:b}".format(i + 1))
-
-    return a
+from tkinter import *
 
 
-def cardsgen(num, b):  # função geradora de cartões
+class Application:  # inicia a aplicação
+    def __init__(self, master=None):
+        self.text_pense = None
+        self.fontePadrao = ("Arial", "10")
+        self.primeiroContainer = Frame(master)
+        self.primeiroContainer["pady"] = 10
+        self.primeiroContainer.pack()
 
-    cards = []  # cria matriz cartão
-    for i in range(num):
-        cards.append([])
+        self.segundoContainer = Frame(master)
+        self.segundoContainer["padx"] = 20
+        self.segundoContainer.pack()
 
-    for i in range(len(b)):  # interar sobre a lista dos binários
-        binarioat = b[len(b) - i - 1]  # binário atual decrescente
-        n_card = len(binarioat)  # tamanho do binário
-        for a in binarioat:  # interar sobre cada elemento binário
-            if a == '1':  # verificar se o número pertence a tal posição
+        self.terceiroContainer = Frame(master)
+        self.terceiroContainer["padx"] = 20
+        self.terceiroContainer.pack()
 
-                cards[n_card - 1].append(len(b) - i)  # adiciona o número a tal cartão
-            n_card = n_card - 1
+        self.quartoContainer = Frame(master)
+        self.quartoContainer["pady"] = 20
+        self.quartoContainer.pack()
 
-    return cards
+        self.titulo = Label(self.primeiroContainer, text="Programa Advinho")
+        self.titulo["font"] = ("Arial", "10", "bold")
+        self.titulo.pack()
+
+        self.num_cards = Label(self.segundoContainer, text="Insira o número de cartões", font=self.fontePadrao)
+        self.num_cards.pack(side=LEFT)
+
+        self.inp_num_cards = Entry(self.segundoContainer)
+        self.inp_num_cards["width"] = 30
+        self.inp_num_cards["font"] = self.fontePadrao
+        self.inp_num_cards.pack(side=LEFT)
+
+        self.gen = Button(self.quartoContainer)
+        self.gen["text"] = "Gerar"
+        self.gen["font"] = ("Calibri", "8")
+        self.gen["width"] = 12
+        self.gen["command"] = self.gerarCards
+        self.gen.pack()
+
+        self.mensagem = Label(self.quartoContainer, text="", font=self.fontePadrao)
+        self.mensagem.pack()
+
+    def binvet(self, n_max):  # Cria uma lista de Binários
+        a = []
+        for i in range(0, n_max):
+            a.append("{0:b}".format(i + 1))
+
+        return a
+
+    def cardsgen(self, num, b):  # função geradora de cartões
+        cards = []  # cria matriz cartão
+        for i in range(num):
+            cards.append([])
+
+        for i in range(len(b)):  # interar sobre a lista dos binários
+            binarioat = b[len(b) - i - 1]  # binário atual decrescente
+            n_card = len(binarioat)  # tamanho do binário
+            for a in binarioat:  # interar sobre cada elemento binário
+                if a == '1':  # verificar se o número pertence a tal posição
+                    cards[n_card - 1].append(len(b) - i)  # adiciona o número a tal cartão
+                n_card = n_card - 1
+
+        return cards
+
+    def gerarCards(self):
+
+        n_cartoes = self.inp_num_cards.get()
+        n_cartoes = int(n_cartoes)
+
+        n_max = 2 ** n_cartoes - 1  # Verifica o número maximo possivel
+
+        self.text_pense = Label(self.terceiroContainer, text="Agora pense em um número de 1 a {n_max}",
+                                font=self.fontePadrao)
+        self.text_pense.pack(side=LEFT)
+
+        vetorBinarios = self.binvet(n_max)  # retorna lista de número em binário
+        cartoes = self.cardsgen(n_cartoes, vetorBinarios)  # Retorna os Cartões
+
+        print("Agora pense em um número de 1 a ", n_max)
+        ja_pensou = input('E ai, já pensou? (S/N) ')
+
+        if ja_pensou.upper() != "S":
+            print('Pois ande logo!')
+        else:
+            print('Não vai esquecer o número...')
+
+        a = 0
+        for i in range(len(cartoes)):
+            b = str(cartoes[i])
+            R = input("Seu número está aqui/n? s\n" + b)
+            if R == 's':
+                a = a + 2 ** i
+        print("Seu número é", a)
+
+        correto = input(f'Acertei? (S/N)')
+        if correto.upper() == "S":
+            print('Eu sou demais!!! ;-)')
 
 
-n_cartoes = int(input("Insira o número de cartões "))  # receber número de cartões
-n_max = 2 ** n_cartoes - 1  # Verifica o número maximo possivel
-vetorBinarios = binvet(n_max)  # retorna lista de número em binário
-cartoes = cardsgen(n_cartoes, vetorBinarios)  # Retorna os Cartões
-
-print("Agora pense em um número de 1 a ", n_max)
-ja_pensou = input('E ai, já pensou? (S/N) ')
-
-if ja_pensou.upper() != "S":
-    print('Pois ande logo!')
-else:
-    print('Não vai esquecer o número...')
-a = 0
-for i in range(len(cartoes)):
-    b = str(cartoes[i])
-    R = input("O numero está aqui? (S/N) -> " + b)
-    if R == 's':
-        a = a + 2 ** i
-print("Seu número é", a)
-
-correto = input(f'Acertei? (S/N)')
-if correto.upper() == "S":
-    print('Eu sou demais!!! ;-)')
+root = Tk()
+Application(root)
+root.mainloop()
--- Estrutura de Decisao/Questões 22 a 28/q28.py
+++ Estrutura de Decisao/Questões 22 a 28/q28.py
@@ -1,9 +1,56 @@
-print("Escolha a sua opção de Carnes")
-
 print("Escolha a sua opção de Carnes "
       "\n 1 - Filé Duplo"
       "\n 2 - Alcatra"
       "\n 3 - Picanha")
+total = 0
+opcao = int(input())
+if opcao == 1:
+    print("Uau Filé Duplo")
+elif opcao == 2:
+    print("Caracas Alcatra com molho é uma delícia")
+elif opcao == 3:
+    print("Você não é franco não hein!!!")
 
-print("Seu cartão é tabajara cliente")
+quantidade = int(input("Qual a quantidade de carnes meu padrão deseja"))
 
+
+def forma_pagamento(total=None):
+    print("Qual a forma de pagamento? 1 - Cartão, 2 - Dinheiro, 3 - Pix")
+    fp = int(input())
+    if fp == 1:
+        print("Seu cartão é Tabajara Cliente? (S/N)")
+        resposta = input()
+        if resposta.upper() == "S":
+            total = total - (total * 0.05)
+            print(total)
+        elif fp == 2 or 1:
+            print("Essa forma de pagamento não possui desconto")
+            print(total)
+
+
+if opcao == 1:
+    valor_carne_ate_5 = 4.9
+    valor_carne_maior_5 = 5.8
+    if quantidade <= 5:
+        total = quantidade * valor_carne_ate_5
+        print(total)
+    else:
+        total = quantidade * valor_carne_maior_5
+        print(total)
+    forma_pagamento(total)
+elif opcao == 2:
+    valor_file_ate_5 = 5.9
+    valor_file_maior_5 = 6.8
+    if quantidade <= 5:
+        total = quantidade * valor_file_ate_5
+    else:
+        total = quantidade * valor_file_maior_5
+    forma_pagamento(total)
+elif opcao == 3:
+    valor_picanha_ate_5 = 6.9
+    valor_picanha_maior_6 = 7.8
+    if quantidade <= 5:
+        total = total * valor_picanha_ate_5
+    else:
+        total = total * valor_picanha_maior_6
+    forma_pagamento(total)
